5d0dc0a76df57cc022d4bce81d3d50dd02b34803
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-01-26 19:16:25 -0300

    cria entidades

---
+++ api/src/main/java/com/eventostec/api/domain/event/address/Address.java
@@ -0,0 +1,28 @@
+package com.eventostec.api.domain.event.address;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String city;
+    private Integer uf;
+
+    @ManyToMany
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ api/src/main/java/com/eventostec/api/domain/event/coupon/Coupon.java
@@ -0,0 +1,26 @@
+package com.eventostec.api.domain.event.coupon;
+
+import com.eventostec.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String code;
+    private Integer discount;
+    private Data valid;
+
+    @ManyToMany
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
